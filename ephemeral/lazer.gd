cfde6809a95e9d5a20e7ba7c7f5a0a5f95ad8a96
alvaropfn <alvaropfn@gmail.com>
2024-02-18 19:25:24 -0300

    partial: change lazer logic

--- ephemeral/lazer.gd
+++ ephemeral/lazer.gd
@@ -1,13 +1,9 @@
-extends Node2D
+extends PhysicsBody2D
 
 @export var lazer_speed = 10
 
-func _ready():
-	var mouse_pos= get_global_mouse_position()
-	var target_rotation = atan2(mouse_pos.y - global_position.y, mouse_pos.x - global_position.x)
-	#target_rotation += deg_to_rad(45)
-	rotation = target_rotation
-	
-
 func _physics_process(_delta):
-	position = Vector2(position.x + lazer_speed, position.y + lazer_speed)
+	move_to_front()
+	#velocity = rotation * lazer_speed
+	#move_to_front()
+	#position += lazer_speed * delta
--- player/player.gd
+++ player/player.gd
@@ -3,14 +3,18 @@ extends CharacterBody2D
 @export var walk_speed = 200
 @export var sprint_speed = 400
 
+var laser_scene: PackedScene = preload("res://ephemeral/lazer.tscn")
+
 func _ready():
 	rotate(PI)
 
 func _physics_process(_delta):
 	handle_movement()
-	handle_rotation()
+	handle_main_weapon()
+
 
 func handle_movement():
+	look_at(get_global_mouse_position())
 	var direction = Vector2()
 	if Input.is_action_pressed("move_up"): direction.y -= 1
 	if Input.is_action_pressed("move_right"): direction.x += 1
@@ -23,15 +27,19 @@ func handle_movement():
 	
 	velocity = direction.normalized() * speed
 	move_and_slide()
-	
-func handle_rotation():
-	var mouse_pos= get_global_mouse_position()
-	var target_rotation = atan2(mouse_pos.y - global_position.y, mouse_pos.x - global_position.x)
-	target_rotation += deg_to_rad(90)
-	rotation = target_rotation
-	
+
+
 func handle_main_weapon():
 	if Input.is_action_pressed("click_primary"):
 		var nozzle_positions = $NozzleArray.get_children()
+		var lazer = laser_scene.instantiate() as PhysicsBody2D
+		var nozzle = nozzle_positions[randi() % nozzle_positions.size()] as Node2D
+		var root_scene = get_parent()
+		
+		lazer.position = nozzle.global_position
+		lazer.look_at(get_global_mouse_position())
+		root_scene.add_child(lazer)
+		
+		
 		
 		
