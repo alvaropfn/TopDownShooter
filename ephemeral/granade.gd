a6bb619651aa5dec0c07f041de23d4a1a3a77a34
alvaropfn <alvaropfn@gmail.com>
2024-02-20 08:40:31 -0300

    partial: improuve lazer scene to appear behind player

--- ephemeral/granade.gd
+++ ephemeral/granade.gd
@@ -1,13 +1,12 @@
 extends RigidBody2D
 
-@export var spd = 1200
+@export var spd = 300
 @export var dir = Vector2.UP
 
 func _ready():
 	rotate(dir.angle())
 	gravity_scale = 0
-	mass = 0
 
 
-func _physics_process(_delta):
-	linear_velocity = dir * spd
+func _physics_process(delta):
+	linear_velocity = dir * spd * delta
--- player/player.gd
+++ player/player.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 @export var wlk_spd = 200
 @export var run_spd = 400
+var can_shoot = true
+var can_granade = true
 
 var laser_scene: PackedScene = preload("res://ephemeral/lazer.tscn")
+var granade_scene: PackedScene = preload("res://ephemeral/granade.tscn")
 
 func _ready():
 	rotate(PI)
@@ -11,6 +14,7 @@ func _ready():
 func _physics_process(_delta):
 	handle_movement()
 	handle_main_weapon()
+	handle_side_weapon()
 
 
 func handle_movement():
@@ -28,16 +32,42 @@ func handle_movement():
 	velocity = direction.normalized() * spd
 	move_and_slide()
 
+func get_player_direction():
+	return (get_global_mouse_position() - position).normalized()
+
 
 func handle_main_weapon():
-	if Input.is_action_pressed("click_primary"):
+	if Input.is_action_pressed("click_primary") and can_shoot:
+		can_shoot = false
+		$LazerTimer.start()
+		
 		var nozzle_positions = $NozzleArray.get_children()
 		var lazer = laser_scene.instantiate() as RigidBody2D
 		var nozzle = nozzle_positions[randi() % nozzle_positions.size()] as Node2D
-		var root_scene = get_parent()
+		var root_scene = get_parent().get_node("./Projectiles")
 		
 		lazer.position = nozzle.global_position
-		lazer.dir = (get_global_mouse_position() - position).normalized()
+		lazer.dir = get_player_direction()
 		root_scene.add_child(lazer)
 
 
+func handle_side_weapon():
+	if Input.is_action_pressed("click_secondary") and can_granade:
+		can_granade = false
+		$GranadeTimer.start()
+		
+		var nozzle = $NozzleArray/Nozzle1
+		var granade = granade_scene.instantiate()
+		var root_scene = get_parent().get_node("./Projectiles")
+		
+		granade.position = nozzle.global_position
+		granade.dir = get_player_direction()
+		root_scene.add_child(granade)
+
+
+func _on_lazer_timer_timeout():
+	can_shoot = true
+
+
+func _on_granade_timer_timeout():
+	can_granade = true
