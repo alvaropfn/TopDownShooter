d9a67a008a3f28e322cf062fff84c12b17b18dba
alvaropfn <alvaropfn@gmail.com>
2024-05-06 14:07:36 -0300

    feat: tween camera animation

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@export var ZOOM = {
+	close = 1,
+	open = 0.65,
+	duration = 1
+}
+
 func _on_gate_gate_entered(body):
 	print("gate entered by: ", body)
 
@@ -16,3 +22,13 @@ func _physics_process(_delta):
 	var speed = 0.1
 	
 	cam.offset = lerp(current, target, speed)
+
+
+func _on_house_player_entered(_body):
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(ZOOM.close, ZOOM.close), ZOOM.duration)
+
+
+func _on_house_player_exited(_body):
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(ZOOM.open, ZOOM.open), ZOOM.duration)
